e53e6911a0f1c28e70a5fda6e27f74796af35be6
Shaheer Ahmed <shaheerabb@gmail.com>
2021-12-09 15:29:13 +0500

    code fix and UI to be added

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # openweather-api-python
-# openweather-api-python
-# openweather-api-python
--- weather.py
+++ weather.py
@@ -1,53 +1,81 @@
 from os import read
-import tkinter as tk
+from tkinter import *
+from typing import Text
 import requests
 import time
 
-with open("D:\Learning and Projects\OW API\key_api.txt") as f:
-             line = f.read()
+with open("D:\Learning and Projects\OW API - Copy\key_api.txt") as f:
+             line = f.readline()
 
-def getWeather(canvas):
+#def click():
+# entered_text=cityName.get()
 
+
+def getWeather(window):
+
+    city = cityName.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + line
+    json_data = requests.get(api).json()
     
-    city = textfield.get()
-    
-    api_ow = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + line
-    json_data = requests.get(api_ow).json()
-    
-    condition = json_data['weather'][0]['main']
+    condition = json_data['weather'][0]['description'].title()
+    infoCity = json_data['name']
+    infoCountry = json_data['sys']['country']
     temp = int(json_data['main']['temp'])
     min_temp = int(json_data['main']['temp_min'])
     max_temp = int(json_data['main']['temp_max'])
     pressure = json_data['main']['pressure']
     humidity = json_data['main']['humidity']
+    visibility = json_data['visibility']
+    cloud = json_data['clouds']['all']
+
     wind = json_data['wind']['speed']
+
     sunrise = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunrise'] - 21600))
     sunset = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunset'] - 21600))
 
-    final_info = condition + "\n" + str(temp) + "°C"
-    final_data = "\n" + "Max Temp: " + str(max_temp) + " °C" + "\n" + "min Temp: " + str(min_temp) + " °C"  + "\n" + "Pressure: " + str(pressure)  + "\n" + "Humidity: " + str(humidity) + "%" + "\n" + "Wind Speed: " + str(wind) + " km/h" + "\n" + "Sunrise: " + str(sunrise) + " AM" + "\n" +  "Sunset: " + str(sunset)  + " PM"
+    CountryData = infoCity + ", " + infoCountry
+
+    final_info = condition + ", " + str(temp) + "°C"
+    final_data = "\n" + "Max Temp: " + str(max_temp) + " °C\n" + "Min Temp: " + str(min_temp) + " °C\n" +  "Pressure: " + str(pressure)  + " hPa\n" +  "Humidity: " + str(humidity) + "%\n" +   "Visibility: " + str(visibility) + "%\n" +  "Cloudiness: " + str(cloud) + "%\n" +  "Wind Speed: " + str(wind) + " ms\n" +  "Sunrise: " + str(sunrise) + " AM\n" +  "Sunset: " + str(sunset)  + " PM"
+    
+    CityLabel.config(text = CountryData)
     label1.config(text = final_info)
     label2.config(text = final_data)
 
-canvas = tk.Tk()
-
-canvas.geometry("550x500")
-canvas.title("Weather App")
-
-f = ("poppins", 15, "bold")
-t = ("poppins", 35, "bold")
-
-textfield = tk.Entry(canvas, justify='center', font = t)
-textfield.pack(pady = 20)
-textfield.focus()
-textfield.bind('<Return>', getWeather)
 
 
-label1 = tk.Label(canvas, font = t)
+# window is the window
+window = Tk()
+window.geometry("550x500")
+window.title("Weather App")
+window.configure(background="black")
+
+# font style
+#f = ("Neue Helvetica", 15)
+#t = ("Neue Helvetica", 35, "bold")
+
+
+#Textfield for the input
+cityName = Entry(window, justify='center', font = ("Neue Helvetica", 35, "bold"), bg="grey", fg="white")
+cityName.pack(pady = 20)
+cityName.focus()
+cityName.bind('<Return>', getWeather)
+
+#tk.Button(window, text="SUBMIT", width=6, command=click)
+
+
+# City info
+CityLabel = Label(window, font = ("Neue Helvetica", 25), bg="black", fg="white", justify="left")
+CityLabel.pack()
+
+# Label1 shows the weather centigrade
+label1 = Label(window, font = ("Neue Helvetica", 30), bg="black", fg="white", justify="left")
 label1.pack()
 
-label2 = tk.Label(canvas, font = f)
+# Label2 shows the more Weather info
+label2 = Label(window, font = ("Neue Helvetica", 15), bg="black", fg="white", justify="left")
 label2.pack()
 
-canvas.mainloop()
+#main loop
+window.mainloop()
 
